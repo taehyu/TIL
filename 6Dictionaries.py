f16505eb264110f198fb7167bec561555910ab35
taehyu <taehyu@hotmail.com>
2020-05-07 10:21:28 +0900

    added table of contents to 6 Dicionaries

--- 6Dictionaries.py
+++ 6Dictionaries.py
@@ -13,3 +13,36 @@ each key is connected to a value
 can use a key to access the value associated with that key
 a key's value can be a number, a string, a list, or even another dictionary
 
+2.1 Accessing Values in a Dictionary
+
+2.2 Adding New Key-Value Pairs
+
+2.3 Starting with an Empty Dictionary
+
+2.4 Modifying Values in a Dictionary
+
+2.5 Removing Key-Value Pairs
+
+2.6 A Dictioanry of Similar Objects
+
+2.7 Using get() to Access Values
+
+3.0 Looping through a Dictionary  
+
+3.1 Looping through All Key-Value Pairs           
+
+3.2 Looping through all the Keys in a Dictionary  
+
+3.3 Looping through a dictionary's keys in a particular order
+
+3.4 Looping through all values in a dictionary
+
+4.0 Nesting
+
+4.1 A List of Dictionaries
+
+4.2 A List in a Dictionary
+
+4.3 A Dictionary in a Dictionary
+
+

315d153b0aa2ead7b7151f1c9d434c61aafd67ad
taehyu <taehyu@hotmail.com>
2020-05-07 10:13:58 +0900

    91~92

---
+++ 6Dictionaries.py
@@ -0,0 +1,15 @@
+
+6.0 dictionaries    
+dictionaries : connect pieces of related info
+
+1.0 A Simple Dictionary 
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+2.0 Working with Dictionaries
+a dictionary in python is a collection of key-value pairs
+each key is connected to a value
+can use a key to access the value associated with that key
+a key's value can be a number, a string, a list, or even another dictionary
+

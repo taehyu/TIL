2c9716275f09bdbfbca93a8c390449de5837c1f9
taehyu <taehyu@hotmail.com>
2020-05-08 11:02:53 +0900

    added 9Classes.py

---
+++ 8Functions.py
@@ -0,0 +1,55 @@
+
+1.0 Defining a Function
+
+1.1 Passing Information to a Function
+
+1.2 Arguments and Parameters
+
+2.0 Passing ArgumentParser
+
+2.1 Positional ArgumentParser
+  
+2.2 Keyword Arguments 
+
+2.3 Default Values  
+
+2.4 Equivalent Function Calls  
+
+2.5 Avoiding Argument Errors  
+
+3.0 Return Values  
+
+3.1 Returning a Simple Value  
+
+3.2 Making an Argument Optional  
+
+3.3 Returning a Dictionary  
+
+3.4 Using a Function with a while Loop 
+
+4.0 Passing a List 
+
+4.1 Modifying a List in a Function  
+
+4.2 Preventing a Function from Modifying a List   
+
+5.0 Passing an Arbitrary Number of Arguments  
+
+5.1 Mixing Positional and Arbitrary Arguments  
+
+5.2 Using Arbitrary Keyword Arguments  
+
+6.0 Storing Your Functions in Modules
+
+6.1 Importing an Entire Module  
+
+6.2 Importing Specific Functions  
+
+6.3 Using as to Give a Function an Alias        
+
+6.4 Using as to Give a Module an Alias        
+
+6.5 Importing All Functions in a Module  
+
+7.0 Styling Functions
+
---
+++ 9Classes.py
@@ -0,0 +1,22 @@
+
+1.0 Creating and Using a Class    
+
+1.1 Creating the Dog Class
+
+1.2 Making an Instance from a Class   
+
+2.0 Working with Clases and Instances  
+
+2.1 The Car Class            
+
+2.2 Setting a Default Value for an Attribute  
+
+2.3 Modifying Attribute Values
+
+3.0 Inheritance
+
+3.1 The __init__() Method for a Child Class
+
+3.2 Defining Attributes and Methods for the Child Class
+
+3.3 Overriding Methods from the Parent Class

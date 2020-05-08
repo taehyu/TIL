3798310839ecb835d243c2618fcae018407ba11a
taehyu <taehyu@hotmail.com>
2020-05-08 11:00:57 +0900

    edited 6 dictionaries.py

--- 6Dictionaries.py
+++ 6Dictionaries.py
@@ -150,6 +150,14 @@ nesting: storing multiple dictionaries in a list / a list of items as a value in
 
 4.1 A List of Dictionaries
 
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+  print(alien)
 
 
 
